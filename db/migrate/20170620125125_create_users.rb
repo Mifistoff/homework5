4ad489b768e68a9d974bfad828077d513da3331d
Michael Tarasyuk <michael.tarasyuk91@gmail.com>
2017-06-20 16:33:42 +0300

    add pole access_level for users db

--- db/migrate/20170620125125_create_users.rb
+++ db/migrate/20170620125125_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :login
       t.string :password
+      t.boolean :access_level
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20170620125125) do
   create_table "users", force: :cascade do |t|
     t.string "login"
     t.string "password"
+    t.boolean "access_level"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
