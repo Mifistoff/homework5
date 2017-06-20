2a7045f38dde085739f8629f02fa43bb6dd8b811
Michael Tarasyuk <michael.tarasyuk91@gmail.com>
2017-06-20 16:54:40 +0300

    added default users and evetns in db from seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(login: 'admin', password: 'admin', access_level: true)
+User.create(login: 'user', password: 'user', access_level: false)
+
+Event.create(name: 'BBQ', place: 'Center', date: '2017-06-25', description: 'BBQ desciption')
+Event.create(name: 'Pizza-Day', place: 'Someweare', date: '2017-06-29', description: 'Greating all on Pizza-Day')
