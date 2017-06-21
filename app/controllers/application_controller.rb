24334b522b908c2085e0fd151e641d62460c3ee4
Michael Tarasyuk <michael.tarasyuk91@gmail.com>
2017-06-21 14:14:29 +0300

    Add current user login on leyout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,9 +9,9 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  def user_login
-    @current_user.login
+  def user_info
+    { id: session[:user_id], login: @current_user.login, role: @current_user.role }
   end
 
-  helper_method :logged_in?, :user_login
+  helper_method :logged_in?, :user_info
 end
